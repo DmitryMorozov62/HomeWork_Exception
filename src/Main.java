8d40ea8f823db7b76f14fc4f0017a305423f777e
DmitryMorozov62 <62morozov.dima.90@gmail.com>
2023-03-22 14:13:16 +0300

    Домашняя работа по Exception(вариант с сообщениями в исключениях)

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,14 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(checkin( "Dmitry_Morozov62","Dima_morozov1234","Dima_morozov1234"));
+        System.out.println(checkin( "Dima_morozov62","Dima_morozov12344","Dima_morozov1234"));
     }
 
     public static boolean checkin(String login, String password, String configPassword) {
         try {
             return checkRegistration(login,password,configPassword);
-        } catch (WrongPasswordException e) {
-            System.out.println("Пароли не совпадают");
-            return false;
-        } catch (WrongLoginException e) {
-            System.out.println("Login и password должны быть меньше 20 симовлов");
+        } catch (WrongPasswordException | WrongLoginException e) {
+            e.printStackTrace();
             return false;
         } catch (NullPointerException e) {
             System.out.println("заполните все поля");
@@ -20,9 +17,9 @@ public class Main {
     }
     public static boolean checkRegistration(String login, String password, String configPassword) throws WrongLoginException, WrongPasswordException {
         if (login.length()>20 || password.length()>20){
-            throw new WrongLoginException();
+            throw new WrongLoginException("Login и password должны быть меньше 20 симовлов");
         } else if(!password.equals(configPassword)) {
-            throw new WrongPasswordException();
+            throw new WrongPasswordException("Пароли не совпадают");
         } else if (!login.matches("^[a-zA-Z0-9-_]+$") || !password.matches("^[a-zA-Z0-9-_]+$")) {
             System.out.println("login и password должны содержать в себе только латинские буквы, цифры и знак подчеркивания.");
             return false;
--- src/WrongLoginException.java
+++ src/WrongLoginException.java
@@ -1,4 +1,8 @@
 public class WrongLoginException extends Exception{
     public WrongLoginException() {
     }
+
+    public WrongLoginException(String message) {
+        super(message);
+    }
 }
