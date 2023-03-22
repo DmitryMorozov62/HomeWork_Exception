a58482f40831023c83ff480def0aeae488e47446
DmitryMorozov62 <62morozov.dima.90@gmail.com>
2023-03-22 13:31:39 +0300

    Домашняя работа по Exception

--- src/Main.java
+++ src/Main.java
@@ -6,24 +6,26 @@ public class Main {
 
     public static boolean checkin(String login, String password, String configPassword) {
         try {
-            checkRegistration(login,password,configPassword);
-            return true;
+            return checkRegistration(login,password,configPassword);
         } catch (WrongPasswordException e) {
-            System.out.println("Пароли не совпадают");;
+            System.out.println("Пароли не совпадают");
             return false;
         } catch (WrongLoginException e) {
-            System.out.println("Login и password должны быть меньше 20 симовлов и содержать в себе только латинские буквы, цифры и знак подчеркивания.");
+            System.out.println("Login и password должны быть меньше 20 симовлов");
             return false;
         } catch (NullPointerException e) {
-            System.out.println("заполните все поля");;
+            System.out.println("заполните все поля");
             return false;
         }
     }
     public static boolean checkRegistration(String login, String password, String configPassword) throws WrongLoginException, WrongPasswordException {
-        if (login.length()>20 || password.length()>20|| !login.matches("^[a-zA-Z0-9-_]+$") || !password.matches("^[a-zA-Z0-9-_]+$")){
+        if (login.length()>20 || password.length()>20){
             throw new WrongLoginException();
         } else if(!password.equals(configPassword)) {
             throw new WrongPasswordException();
+        } else if (!login.matches("^[a-zA-Z0-9-_]+$") || !password.matches("^[a-zA-Z0-9-_]+$")) {
+            System.out.println("login и password должны содержать в себе только латинские буквы, цифры и знак подчеркивания.");
+            return false;
         }
         return true;
     }
